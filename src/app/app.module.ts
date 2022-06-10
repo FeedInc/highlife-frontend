3b1f7b009b46fbf895b0872322fe5cf33da4e490
adrianesqueiros <u201718169@upc.edu.pe>
2022-06-10 01:26:24 -0500

    feature: feature home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule} from "@angular/fire/compat";
+import { environment } from 'src/environments/environment';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+// Your web app's Firebase configuration
+  firebaseConfig :{
+    apiKey: "AIzaSyCDnBxm8FAFiNwMMallEzr9d5lGJFUpGyM",
+    authDomain: "highlife-web.firebaseapp.com",
+    projectId: "highlife-web",
+    storageBucket: "highlife-web.appspot.com",
+    messagingSenderId: "630971348324",
+    appId: "1:630971348324:web:d4fbc633df5e132fa0b671"
+  }
+
+
 };
 
 /*
